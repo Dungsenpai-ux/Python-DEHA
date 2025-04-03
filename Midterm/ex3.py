e90d93e593387829c8b2dabdbe04a0539bd429ed
Dungsenpaii <phamtrungtruc03@gmail.com>
2025-04-03 14:51:31 +0700

    Update ex3.py

--- Midterm/ex3.py
+++ Midterm/ex3.py
@@ -1,9 +1,13 @@
 def bubble_sort(arr):
     n = len(arr)
-    for i in range(n):
-        for j in range(0, n-i-1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(1, n):
+            if arr[i-1] > arr[i]:
+                arr[i-1], arr[i] = arr[i], arr[i-1]
+                swapped = True
+        n -= 1
 
 arr = list(map(int, input("Nhập vào một mảng số nguyên, cách nhau bởi dấu cách: ").split()))
 bubble_sort(arr)
